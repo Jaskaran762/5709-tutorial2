9809c15569ac2f55f254aff9936a3f7004eb8b3e
Jaskaran Singh <js356337@dal.ca>
2024-03-11 09:41:58 -0300

    fix for routing issue

--- tutorial5/functions/app.js
+++ tutorial5/functions/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // Routes
-app.use('/users', userRoutes);
+app.use('/.netlify/functions/app/users', userRoutes);
 
 /* // Start the server
 const PORT = process.env.PORT || 3000;
